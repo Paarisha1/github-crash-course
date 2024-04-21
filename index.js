fc1fd11912d89606b38f3c9ab3418e11dda59dd2
[Paarisha Kaushik] <scin.paarishakaushik27@gmail.com>
2024-04-21 16:57:20 +0530

    commited

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const age =24;
-const  name='Paarisha';
+const  name1='payal'
 const dob='01/01/2002';
+function sayName(){
+    console.log('My name is'+ name1)
+}
+function isAbove(){
+    return age>=18
+}
